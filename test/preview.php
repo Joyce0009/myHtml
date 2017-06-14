7d3ceeb7359b94a6213fcba87348b6346375aad2
Joyce_Lin <joyce_lin@asus.com>
2017-06-14 16:20:24 +0800

    Image preview - Add image info table

--- test/preview.php
+++ test/preview.php
@@ -7,10 +7,19 @@
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = function(e) {
-                    $('#preview').css("display", "block");
+                    var img = new Image();
+                    img.onload = function() {
+                        console.log('w ' + this.width);
+                        console.log('h ' + this.height);
+                    };
+                    img.src = e.target.result;
+                    $('#preview').css("display", "inline");
                     $('#imagePreview').attr('src', e.target.result);
                 };
                 reader.readAsDataURL(input.files[0]);
+                console.log('filename ' + input.files[0].name);
+                console.log('size ' + input.files[0].size);
+                console.log('type ' + input.files[0].type);
             }
         }
 
@@ -20,23 +29,48 @@
         }
     </script>
     <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        td, th {
+            border: 1px solid #badcff;
+            text-align: left;
+            padding: 8px;
+        }
+        tr:nth-child(even) {
+            background-color: #d3e9ff
+        }
+        tr:nth-child(odd) {
+            background-color:#ffffff;
+        }
         #preview {
-            width: 140px;
+            width: 500px;
             display: none;
             position: relative;
         }
         #imagePreview {
-            width: 140px;
+            width: 100px;
             border: 1px solid #e4d3c3;
             padding: 8px;
             float: left;
+            display: inline-block;
         }
         #deleteimg {
             cursor: pointer;
             width: 30px;
             position: absolute;
             top: -15px;
-            right: -35px;
+            right: -15px;
+        }
+        #imageInfo {
+            width: 100%;
+            font-family: arial, sans-serif;
+            position: absolute;
+            display: inline-block;
+            border-collapse: collapse;
+            padding-left: 30px;
         }
     </style>
     <body>
@@ -56,6 +90,12 @@
                             <div id="preview">
                                 <img id="imagePreview" src="">
                                 <img id="deleteimg" src="delete.png" onclick="removeFile();">
+                                <div id="imageInfo">
+                                    <table>
+                                        <tr><th>Type</th><th>Size(MB)</th><th>W/H(px)</th><th>Filename</th></tr>
+                                        <tr><td>png</td><td>10</td><td>1280/1920</td><td>123.png</td></tr>
+                                    </table>
+                                </div>
                             </div>
                         </fieldset>
                    </td>
