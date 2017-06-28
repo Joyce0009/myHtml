d97751b1f575e8c9459548210eadb57b33283c7e
Joyce_Lin <joyce_lin@asus.com>
2017-06-28 18:32:46 +0800

    Add rename and upload file

--- test/action_page.php
+++ test/action_page.php
@@ -1,79 +1,79 @@
 <?php
-    // define variables and set to empty values
-    $titleErr = "";
-    $title = "";
+    $IS_DEBUG = true;
+    if ($IS_DEBUG) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+    }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-        $checkResult = checkInputTitle() && checkImage($target_file, $imageFileType);
-
-        // Check if $uploadOk is set to 0 by an error
-        if (!$checkResult) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
+        if ($IS_DEBUG) {
+            foreach($_POST as $key => $entry) {
+                if (is_array($entry)) {
+                    print $key . ": " . implode(',', $entry) . "<br>";
+                } else {
+                    print $key . ": " . $entry . "<br>";
+                }
             }
         }
+
+        $original_name = $_FILES['fileToUpload']['name']; // include extension
+        $extension = strtolower(pathinfo($original_name, PATHINFO_EXTENSION));
+        $temp_name = $_FILES['fileToUpload']['tmp_name'];
+        $size = $_FILES['fileToUpload']['size'];
+        $file_error = $_FILES['fileToUpload']['error'];
+        $response = '';
+
+        switch ($file_error) {
+            case UPLOAD_ERR_OK:
+                $is_valid = true;
+                if (!isset($_POST['submit'])) {
+                    $is_valid = false;
+                    $response = 'Sorry, not set from submit button';
+                } else if (!in_array($extension, array('jpg', 'jpeg', 'png', 'gif'))) {
+                    $is_valid = false;
+                    $response = 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
+                } else if ($size/(1024 * 1024) > 5) {
+                    $is_valid = false;
+                    $response = 'File size is exceeding max allowed size(5M)';
+                }
+                if ($is_valid) {
+                    $target_path = dirname( __FILE__) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_POST["hidden_folder_name"];
+                    $target_file = $target_path . $_POST["hidden_filename"] . "." . $extension;
+                    if (move_uploaded_file($temp_name, $target_file)) {
+                        $response = 'Your file ' . $original_name . ' has been uploaded to '. $target_file;
+                    } else {
+                        $response = 'Sorry, there was an error uploading your file.';
+                    }
+                }
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+                $response = 'The uploaded file exceeds the upload_max_filesize (in php.ini. @See phpInfo())';
+                break;
+            case UPLOAD_ERR_FORM_SIZE:
+                $response = 'The uploaded file exceeds the MAX_FILE_SIZE (specified in the HTML form)';
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $response = 'The uploaded file was only partially uploaded.';
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $response = 'No file was uploaded.';
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $response = 'Missing a temporary folder. Introduced in PHP 4.3.10, 5.0.3';
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $response = 'Failed to write file to disk. Introduced in PHP 5.1.0';
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                $response = 'File upload stopped by extension. Introduced in PHP 5.2.0';
+                break;
+            default:
+                $response = 'Unknown Error';
+                break;
+        }
+
+        echo $response;
     } else {
         echo "Sorry, need POST request!";
-    }
-
-    function correctInputText($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    function checkInputTitle() {
-        $checkResult = true;
-        if (empty($_POST["title"])) {
-            $titleErr = "Title is required.";
-            $checkResult = false;
-            echo $titleErr . "<br>";
-        } else {
-            $title = correctInputText($_POST["title"]);
-            $checkResult = true;
-            echo "Title: " . $title . "<br>";
-        }
-        return $checkResult;
-    }
-
-    function checkImage($target_file, $imageFileType) {
-        $checkResult = true;
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $checkResult = true;
-            } else {
-                echo "File is not an image.";
-                $checkResult = false;
-            }
-        }
-        // Check if file already exists
-//        if (file_exists($target_file)) {
-//            echo "Sorry, file already exists.";
-//            $checkResult = false;
-//        }
-        // Check file size. need small than 500kb
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-            echo "Sorry, your file is too large.";
-            $checkResult = false;
-        }
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $checkResult = false;
-        }
-        return $checkResult;
     }
 ?>
--- test/preview.php
+++ test/preview.php
@@ -3,10 +3,11 @@
     <script src="jquery-3.2.1.min.js"></script>
     <script type="text/javascript">
         var inputFileDimen = {"width": 0, "height": 0};
+        var checkedResult = {"filename": false, "type": false, "dimension": false, "size": false};
 
         // Using jQuery to preivew image
         function filePreview(input) {
-            cleanCheckedResult();
+            clearCheckedResult();
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = function(e) {
@@ -17,11 +18,11 @@
                 var file = input.files[0];
                 reader.readAsDataURL(file);
                 showAndCheckFileInfo(file);
-                showAndCheckExpectedDimen();
+                showAndCheckExpectedDimen(getExpectedDimen());
             }
         }
 
-        function cleanCheckedResult() {
+        function clearCheckedResult() {
             $('#filename_checked').css('display', "none");
             $('#type_checked').css('display', "none");
             $('#size_checked').css('display', "none");
@@ -30,33 +31,76 @@
             $('#type_errMeg').text("");
             $('#size_errMeg').text("");
             $('#dimension_errMeg').text("");
+            $('#destination_folder').val("");
+            $('#destination_file').val("");
             inputFileDimen = {"width": 0, "height": 0};
         }
 
         function removeFile() {
             $('#preview').css("display", "none");
             $('#fileToUpload').val("");
+            $("#submit").prop("disabled", true);
         }
 
         function showAndCheckFileInfo(file) {
             // show
-            $('#filename').text(file.name);
+            var filename = file.name.substr(0, file.name.lastIndexOf('.'));
+            $('#filename').text(filename);
+            $('#destination_file').attr('placeholder', getRecommandFilename(filename));
+            updateHiddenFilename(filename);
+            var selectedResolution = $('#resolution option:selected').text();
+            udpateDesFolderHint(selectedResolution);
+            updateHiddenFolderName(getRecommandFolderName(selectedResolution));
             $('#type').text(file.type);
             // bytes to MB
             $('#size').text((file.size*0.00000095367432).toFixed(2));
 
             // check
-            displayChecked(checkFileName(file.name), 'filename_checked', 'filename_errMeg');
+            displayChecked(checkFileName(filename), 'filename_checked', 'filename_errMeg');
             displayChecked(checkType(file.type), 'type_checked', 'type_errMeg');
             displayChecked(checkSize(file.size), 'size_checked', 'size_errMeg');
         }
 
+        function updateHiddenFolderName(folder_name) {
+            $('#hidden_folder_name').val(folder_name);
+        }
+
+        function updateHiddenFilename(filename) {
+            $('#hidden_filename').val(filename);
+        }
+
         function checkFileName(filename) {
             // TODO check "default_wallpaper", "1a", "1"
+            var checkedResult;
             if (filename.startsWith("default_wallpaper")) {
-                return 0;
+                checkedResult = 0;
             } else {
-                return "Must starts with 'default_wallpaper'";
+                checkedResult = "Must starts with 'default_wallpaper'";
+            }
+
+            updateRenameTextColor(checkedResult);
+            return checkedResult;
+        }
+
+        function updateRenameTextColor(checkedResult) {
+            if (0 === checkedResult) {
+                $('#rename_text').text("Rename:");
+                $('#rename_text').css("color", "black");
+            } else {
+                $('#rename_text').text("Rename*:");
+                $('#rename_text').css("color", "red");
+            }
+        }
+
+        function getRecommandFolderName(selectedResolution) {
+            return '20_phone_' + selectedResolution + '/';
+        }
+
+        function getRecommandFilename(filename) {
+            if (filename.startsWith("asus_phone")) {
+                return filename.replace("asus_phone", "default_wallpaper");
+            } else {
+                return filename;
             }
         }
 
@@ -81,15 +125,6 @@
             }
         }
 
-        function isString(toBeCheckedVar) {
-            // jQuery.type()
-            if ($.type(toBeCheckedVar) === "string") {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
         function checkSize(size) {
             // < 20 MB
             if (size < 20 * 1048576) {
@@ -103,9 +138,36 @@
             if (0 === result) {
                 $('#' + checkedID).attr('src', 'checked.png');
                 $('#' + checkedID).attr('style', 'width: 30px');
+                $('#' + errMsgID).text("");
             } else {
                 $('#' + errMsgID).text(result);
+                $('#' + checkedID).css('display', "none");
             }
+            
+            updatecheckedResult(result, checkedID);
+        }
+
+        function updatecheckedResult(result, checkedID) {
+            if ("filename_checked" === checkedID) {
+                checkedResult.filename = (result === 0) ? true : false;
+            } else if ("type_checked" === checkedID) {
+                checkedResult.type = (result === 0) ? true : false;
+            } else if ("size_checked" === checkedID) {
+                checkedResult.size = (result === 0) ? true : false;
+            } else if ("dimension_checked" === checkedID) {
+                checkedResult.dimension = (result === 0) ? true : false;
+            }
+            $("#submit").prop("disabled", isDisabledSubmit());
+        }
+
+        /**
+         * Checked if all result keys has false values
+         * @returns {Boolean}
+         */
+        function isDisabledSubmit() {
+            // jQuery $.inArray(target, array) is similary to JS's native .indexOf(),
+            // reutn -1 means not find the target
+            return $.inArray(false, $.map(checkedResult, function (obj) {return obj;} )) > -1;
         }
 
         function showImageDimen(src) {
@@ -128,10 +190,9 @@
             return {"width": width.toFixed(0),  "height": height.toFixed(0)};
         }
 
-        function showAndCheckExpectedDimen() {
+        function showAndCheckExpectedDimen(expectedDimen) {
             $('#dimension_checked').css('display', "none");
             $('#dimension_errMeg').text("");
-            var expectedDimen = getExpectedDimen();
             $('#expected_dimension').text(expectedDimen.width + 'x' + expectedDimen.height);
             if (inputFileDimen.width > 0 && inputFileDimen.height > 0) {
                 displayChecked(checkDimension(expectedDimen.width, expectedDimen.height, inputFileDimen.width, inputFileDimen.height),
@@ -140,7 +201,33 @@
         }
 
         function onChangeResolution() {
-            showAndCheckExpectedDimen();
+            var selectedResolution = $('#resolution option:selected').text();
+            udpateDesFolderHint(selectedResolution);
+            updateHiddenFolderName(getRecommandFolderName(selectedResolution));
+            showAndCheckExpectedDimen(getExpectedDimen());
+        }
+
+        function udpateDesFolderHint(selectedResolution) {
+            $('#destination_folder').attr('placeholder', getRecommandFolderName(selectedResolution));
+        }
+
+        function onChangeFolderName() {
+            updateHiddenFolderName($('#destination_folder').val());
+        }
+
+        function onChangeFilename() {
+            var inputName = $('#destination_file').val();
+            updateHiddenFilename(inputName);
+            displayChecked(checkFileName(inputName), 'filename_checked', 'filename_errMeg');
+        }
+
+        function isString(toBeCheckedVar) {
+            // jQuery.type()
+            if ($.type(toBeCheckedVar) === "string") {
+                return true;
+            } else {
+                return false;
+            }
         }
     </script>
     <style>
@@ -179,7 +266,7 @@
             padding: 8px;
             float: left;
             display: inline-block;
-        }
+        }        
         #deleteimg {
             cursor: pointer;
             width: 30px;
@@ -215,13 +302,24 @@
                                 <img id="deleteimg" src="delete.png" onclick="removeFile();">
                                 <div id="imageInfo">
                                     <table>
-                                        <tr><th>Filename</th>
+                                        <tr><th>Destination Folder/File</th>
+                                            <th>Filename</th>
                                             <th>Type</th>
                                             <th>Expected WxH(px)</th>
                                             <th>Actual WxH(px)</th>
                                             <th>Size(MB)</th>
                                         </tr>
-                                        <tr><td><span id="filename"></td>
+                                        <tr><td rowspan="2">
+                                                <input type='hidden' name='hidden_folder_name' id='hidden_folder_name'>
+                                                Folder Path: <input id="destination_folder" type="text" onchange="onChangeFolderName()">
+                                                <hr>
+                                                <text id="rename_text">Rename:</text>
+                                                <input type='hidden' name='hidden_filename' id='hidden_filename'>
+                                                <input id="destination_file" type="text" onchange="onChangeFilename()">
+                                            </td>
+                                            <td>
+                                                <span id="filename"></span>
+                                            </td>
                                             <td><span id="type"></td>
                                             <td>
                                                 <select id="resolution" onchange="onChangeResolution()">
@@ -229,8 +327,8 @@
                                                     <option value="1080_1920" selected=>w1080_h1920</option>
                                                 </select>
                                                 <div>
-                                                    <input type="radio" name="ratio" class="resolution_ratio" value ="1" onchange="onChangeResolution()">1
-                                                    <input type="radio" name="ratio" class="resolution_ratio" value ="1.17" onchange="onChangeResolution()" checked>1.17
+                                                    <input type="radio" name="radio" class="resolution_ratio" value ="1" onchange="onChangeResolution()">1
+                                                    <input type="radio" name="radio" class="resolution_ratio" value ="1.17" onchange="onChangeResolution()" checked>1.17
                                                 </div>
                                                 <div style="text-align: center;">
                                                     <span id="expected_dimension"></span>
@@ -266,7 +364,7 @@
                 </tr>
                 <tr>
                     <td style="text-align:center" colspan="2">
-                        <input type="submit" value="Submit">
+                        <input id="submit" type="submit" name="submit" value="Submit" disabled="true">
                     </td>
                 </tr>
             </table>
